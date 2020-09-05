de87d5dd458719db60ad2bf6b235288693fd2e81
alex-chiu <32287508+alex-chiu@users.noreply.github.com>
2020-09-05 16:15:42 -0500

    Create Text Boxes and Buttons

--- JSHangman/game.js
+++ JSHangman/game.js
@@ -1,1 +1,27 @@
-// JS Code for Hangman Assignment
+// JS Hangman Code
+
+var gameStarted = false;
+var word = "";
+var guessedWord = "";
+
+document.getElementById("start-button").addEventListener("click", startGame);
+document.getElementById("guess-button").addEventListener("click", checkGuess);
+
+function startGame() {
+    if (!gameStarted) {
+        gameStarted = true;
+        word = document.getElementById("input-box").value;
+        word = word.toUpperCase();
+        document.getElementById("input-box").value = "";
+
+        for (var i = 0; i < word.length; i++) {
+            guessedWord = guessedWord.concat("X");
+        }
+
+        document.getElementById("guess-space").innerHTML = guessedWord;
+    }
+}
+
+function checkGuess() {
+    
+}
